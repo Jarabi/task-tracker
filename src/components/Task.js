6e28582d03bcb9e994256974d16c644385ff82fe
Alex Jarabi <jarabialex@gmail.com>
2023-11-05 17:37:38 +0300

    Add task submit

---
+++ src/components/Task.js
@@ -0,0 +1,21 @@
+import { TiTimes } from "react-icons/ti";
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}{" "}
+        <TiTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
